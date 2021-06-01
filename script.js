373f5d86ba43c4d5138f2c34a33fc57f5eec3052
pierrialexander <65265714+pierrialexander@users.noreply.github.com>
2021-06-01 20:32:30 -0300

    :package: form orcamento

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//var nome = window.prompt('Qual é o seu nome? ')
+//window.alert('Seja bem vindo, ' + nome + '!')
+
 document.querySelector('.hamburguer').addEventListener('click', () => {
     document.querySelector('.container').classList.toggle('show-menu')
 })
